babcd7c19542dcfbaa88637ec20266433f6f0722
Sushant Mathur <sushant.post@gmail.com>
2019-09-15 18:44:51 -0700

    Initial commit, both clients working correctly.

---
+++ src/main/java/Client2.java
@@ -0,0 +1,53 @@
+/* @Author: Sushant Amit Mathur. SJSU ID: 014489865*/
+import com.google.gson.Gson;
+import com.squareup.okhttp.*;
+import java.io.IOException;
+import java.util.Date;
+
+public class Client2 {
+    //Set the type of Media for the OkHTTP library
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");;
+
+    //Sends the message with a POST request over HTTP
+    public static void sendHttpRequest() throws IOException {
+        //Get system time, diving by 1000 to be able to store in int.
+        int time_2 = (int) (new Date().getTime()/1000);
+        //Initialise new message with the client's ID and time
+        RoverData rover1 = new RoverData(2,time_2);
+        /* I am using the OkHTTP library to send data
+           here I create a POST request and send it over,
+           parse the response and print the values onto System.out
+         */
+        String local = "http://localhost:8080/update";
+        Gson gson = new Gson();
+        String rover1Json = gson.toJson(rover1);
+        OkHttpClient okHttpClient = new OkHttpClient();
+        RequestBody body = RequestBody.create(JSON, rover1Json);
+        Request request = new Request.Builder().url(local).post(body).build();
+        //Try getting the response and print exception if occurs
+        try{ Response response = okHttpClient.newCall(request).execute();
+            RoverData responseData = gson.fromJson(response.body().string(), RoverData.class);
+            //Print the rover ID and time as we got from the server
+            System.out.println("The response ID is: " + responseData.id+ " The time is: "+
+                    new Date(((long)responseData.time)*1000L));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String args[]) throws InterruptedException {
+        /* Run periodically 10 times with 3 seconds delay
+           between each call. */
+        int i = 0;
+        while(i<10) {
+            Thread.sleep(3000);
+            try {
+                sendHttpRequest();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            i++;
+        }
+    }
+}
+
---
+++ src/main/java/RoverData.java
@@ -0,0 +1,15 @@
+/* @Author: Sushant Amit Mathur. SJSU ID: 014489865*/
+
+/* This is the data that we are sending over the network
+*  and receiving too for now. We may change it later so
+*  will add another class when need be.
+* */
+
+public class RoverData {
+    int id;
+    int time;
+    public RoverData(int id, int time){
+        this.id = id;
+        this.time = time;
+    }
+}
